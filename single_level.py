2eaccf148638ce0cfb9160be75b83a5a72d36020
katger4 <kajgertz@gmail.com>
2017-08-16 19:27:59 -0700

    add fields to evaluate for compliance with the Orbis Cascade Alliance's EAD Best Practices v3.8.1

--- single_level.py
+++ single_level.py
@@ -46,6 +46,15 @@ def get_single_value(resource, key):
 	else:
 		return "false"
 
+def get_values_list(resource, array, value):
+	value_types = []
+	for item in resource[array]:
+		if item[value]:
+			value_types.append(item[value])
+		else:
+			value_types.append("false")
+	return value_types
+
 def makeRow(resource):
 	global row
 	row = []
@@ -53,14 +62,31 @@ def makeRow(resource):
 	title = get_single_value(resource, "title").encode('utf-8')
 	resource_id = get_single_value(resource, "id_0")
 	extent = get_values(resource, "extents", "number")
-	date = get_values(resource, "dates", "date_type")
-	agent = get_values(resource, "linked_agents", "role")
+	date_labels = get_values(resource, "dates", "label")
 	language = get_single_value(resource, "language")
 	repository = get_single_value(resource, "repository")
-	required_values = title, publish, resource_id, extent, date, language
+	level = get_single_value(resource, "level")
+
+	agent = get_values(resource, "linked_agents", "role")
+	subjects = get_values_list(resource, "subjects", "ref")
+	
 	scope = get_note_contents(resource, "notes", "scopecontent")
 	access = get_note_contents(resource, "notes", "accessrestrict")
-	required_notes = scope, access
+	abstract = get_note_contents(resource, "notes", "abstract")
+	bioghist = get_note_contents(resource, "notes", "bioghist")
+
+	ead_id = get_single_value(resource, "ead_id")
+	ead_location = get_single_value(resource, "ead_location")
+	finding_aid_author = get_single_value(resource, "finding_aid_author")
+	finding_aid_date  = get_single_value(resource, "finding_aid_date")
+	finding_aid_description_rules  = get_single_value(resource, "finding_aid_description_rules")
+	finding_aid_filing_title = get_single_value(resource, "finding_aid_filing_title")
+	finding_aid_language = get_single_value(resource, "finding_aid_language")
+	finding_aid_title = get_single_value(resource, "finding_aid_title")
+
+	required_values = title, publish, resource_id, level, extent, language
+	required_notes = abstract, bioghist, scope, access
+	required_ead = ead_id, ead_location, finding_aid_title, finding_aid_author, finding_aid_date, finding_aid_description_rules, finding_aid_filing_title, finding_aid_language
 
 	for item in required_values:
 		if item != "false": 
@@ -72,8 +98,16 @@ def makeRow(resource):
 		response = requests.get(repositoryBaseURL, headers=headers).json()
 		row.append(response["name"])
 	else:
-		row.append("false")			
+		row.append("false")		
 
+	# need at least one creation date
+	if 'creation' in date_labels:
+		row.append(date_labels)
+	else:
+		row.append("false")
+
+
+	# need at least one creator
 	if "creator" in agent:
 		creator_list = []
 		for item in resource["linked_agents"]:
@@ -83,14 +117,36 @@ def makeRow(resource):
 		row.append(", ".join(creator_list).encode('utf-8'))
 	else:
 		row.append("false")
-							
+
+	# add archives west browsing terms if exist (need at least one)
+	# for ref in subjects:
+	# 	print resourceURL+ref
+	# 	#print requests.get(resourceURL + ref, headers=headers).json()
+	source_list = [requests.get('http://localhost:8089' + ref).json()['source'] for ref in subjects]
+	if 'Archives West Browsing Terms' in source_list:
+		sub_list = []
+		for ref in subjects:
+			subject = requests.get(resourceURL + ref, headers=headers).json()
+			if subject['source'] == 'Archives West Browsing Terms':
+				sub_list.append(subject['title'])
+		row.append(", ".join(sub_list).encode('utf-8'))
+	else:
+		row.append("false")
+	
+	# add notes if exist						
 	for item in required_notes:
 		if item:
 			row.append(item)
 		else:
 			row.append("false")
+
+	for item in required_ead:
+		if item != "false": 
+			row.append(item)
+		else:
+			row.append("false")	
 	
-	print row	
+	print "Writing resource ", resource_id,"..."	
 
 def main():
 	#User input to refine functionality of script 
@@ -106,7 +162,7 @@ def main():
 	
 	#Creating csv
 	writer = csv.writer(open(spreadsheet, "wb"))
-	column_headings = ["title", "publish", "resource", "extent", "date", "language", "repository", "creator", "scope", "restrictions"]
+	column_headings = ["title", "publish", "resource", "level", "extent", "date", "language", "repository", "creation_date", "creator", "aw_subjects", "abstract", "bioghist", "scope", "restrictions", 'ead_id', 'ead_location', 'finding_aid_title', 'finding_aid_author', 'finding_aid_date', 'finding_aid_description_rules', 'finding_aid_filing_title', 'finding_aid_language']
 	writer.writerow(column_headings)
 
 	#Checking ALL resources
